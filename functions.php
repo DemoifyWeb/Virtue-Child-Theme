890868a3e1ac94a99b4df7aa1755f3f8561f6cb5
rojita <rojitalamichhane11@gmail.com>
2018-05-13 21:19:13 +0545

    Initial Commit

--- functions.php
+++ functions.php
@@ -2,7 +2,15 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+add_action( 'wp_enqueue_scripts','my_theme_enqueue_styles');
+
+function wpb_load_fa(){
+	wp_enqueue_style('wpb_load_fa',''https://fonts.googleapis.com/css?family=Barlow');
 
 }
+add_action('wp_enqueue_scripts','wpb_load_fa');
+}
 ?>
+
+
 
